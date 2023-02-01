576baeaceecb75d402d29a3f80a626a8a5368ab0
Ignacio Fonts <fontsignacio@gmail.com>
2023-02-01 13:29:28 -0300

    init app with firebase conection

---
+++ android/app/src/main/kotlin/com/ignacio/quiz_flow/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ignacio.quiz_flow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_flow/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MaterialApp(
+    title: "QuizFlow",
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: const Color.fromARGB(255, 29, 15, 29),
+      ),
+      scaffoldBackgroundColor: const Color.fromARGB(255, 29, 15, 29),
+    ),
+    home: const Home(),
+  ));
+}
